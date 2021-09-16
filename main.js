d7d6a4b247753293020ecacb8ad953703dd50a38
audetri <63405381+audetri@users.noreply.github.com>
2021-09-16 20:01:38 +0200

    Messqge randomly generated

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log("Hello");
+let time = ['Today', 'Yesterday', 'Tomorrow', 'Next month'];
+let verb = ['will be', 'could be', 'may be'];
+let compl =['fortunate', 'dangerous', 'promising', 'sad'];
+
+const sentence = {
+    S1: time[Math.floor(Math.random()*time.length)],
+    S2: verb[Math.floor(Math.random()*verb.length)],
+    S3: compl[Math.floor(Math.random()*compl.length)]
+};
+ console.log(`${sentence.S1} ${sentence.S2} ${sentence.S3}`);
